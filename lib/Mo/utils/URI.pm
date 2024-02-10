d8d4f761afc133bb86ae1ae9c0b26bead0cdac7c
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-02-10 15:25:03 +0100

    Add check_url()

--- lib/Mo/utils/URI.pm
+++ lib/Mo/utils/URI.pm
@@ -7,8 +7,9 @@ use warnings;
 use Data::Validate::URI qw(is_uri);
 use Error::Pure qw(err);
 use Readonly;
+use URI;
 
-Readonly::Array our @EXPORT_OK => qw(check_uri);
+Readonly::Array our @EXPORT_OK => qw(check_uri check_url);
 
 our $VERSION = 0.01;
 
@@ -22,6 +23,24 @@ sub check_uri {
 	my $value = $self->{$key};
 	if (! is_uri($value)) {
 		err "Parameter '".$key."' doesn't contain valid URI.",
+			'Value', $value,
+		;
+	}
+
+	return;
+}
+
+sub check_url {
+	my ($self, $key) = @_;
+
+	if (! exists $self->{$key}) {
+		return;
+	}
+
+	my $value = $self->{$key};
+	my $uri = URI->new($value);
+	if (! $uri->can('scheme') || ! $uri->can('host') || ! $uri->scheme || ! $uri->host) {
+		err "Parameter '".$key."' doesn't contain valid URL.",
 			'Value', $value,
 		;
 	}
@@ -43,9 +62,10 @@ Mo::utils::URI - Mo utilities for URI.
 
 =head1 SYNOPSIS
 
- use Mo::utils::URI qw(check_uri);
+ use Mo::utils::URI qw(check_uri check_url);
 
  check_uri($self, $key);
+ check_url($self, $key);
 
 =head1 DESCRIPTION
 
@@ -63,10 +83,24 @@ Put error if check isn't ok.
 
 Returns undef.
 
+=head2 C<check_url>
+
+ check_url($self, $key);
+
+Check parameter defined by C<$key> which is valid URL.
+
+Put error if check isn't ok.
+
+Returns undef.
+
 =head1 ERRORS
 
- check_uri(): 
+ check_uri():
          Parameter '%s' doesn't contain valid URI.
+                 Value: %s
+
+ check_url():
+         Parameter '%s' doesn't contain valid URL.
                  Value: %s
 
 =head1 EXAMPLE1
@@ -112,12 +146,56 @@ Returns undef.
  # Output like:
  # #Error [..utils.pm:?] Parameter 'key' doesn't contain valid URI.
 
+=head1 EXAMPLE3
+
+=for comment filename=check_url_ok.pl
+
+ use strict;
+ use warnings;
+
+ use Mo::utils::URI qw(check_url);
+
+ my $self = {
+         'key' => 'https://skim.cz',
+ };
+ check_url($self, 'key');
+
+ # Print out.
+ print "ok\n";
+
+ # Output:
+ # ok
+
+=head1 EXAMPLE4
+
+=for comment filename=check_url_fail.pl
+
+ use strict;
+ use warnings;
+
+ use Error::Pure;
+ use Mo::utils::URI qw(check_url);
+
+ $Error::Pure::TYPE = 'Error';
+
+ my $self = {
+         'key' => 'bad_uri',
+ };
+ check_uri($self, 'key');
+
+ # Print out.
+ print "ok\n";
+
+ # Output like:
+ # #Error [..utils.pm:?] Parameter 'key' doesn't contain valid URL.
+
 =head1 DEPENDENCIES
 
 L<Data::Validate::URI>,
 L<Error::Pure>,
 L<Exporter>,
-L<Readonly>.
+L<Readonly>,
+L<URI>.
 
 =head1 SEE ALSO
 
